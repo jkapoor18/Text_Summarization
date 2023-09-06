3d4c8db903955a4bf4d3ae9409a11050d7072559
jkapoor18 <jyotikpr999@gmail.com>
2023-09-06 15:58:44 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Text-Summarizer-Project"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "Text_Summarization"
+AUTHOR_USER_NAME = "Jyoti Kapoor"
 SRC_REPO = "textSummarizer"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "jyotikpr999@gmail.com"
 
 
 
